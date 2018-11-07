ade7b28758dabb91f14aa9614e9e6964ff621643
kimutak <euisong2@naver.com>
2018-11-07 18:39:13 +0900

    second

---
+++ 1158.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+typedef struct NODE *node_list;
+struct NODE{
+	node_list next;
+	int data;
+};
+
+void addNode(node_list target, int data)
+{
+	node_list newNode = (node_list*)malloc(sizeof(struct NODE));
+	newNode->next = target->next;
+	newNode->data = data;
+	target->next = newNode;
+}
+
+void removeNode(node_list target)
+{
+	node_list deleteNode= target->next;
+	target->next = deleteNode->next;
+	
+	free(deleteNode);
+}
+
+int main(int argc, char *argv[]) {
+	int i, j, n, m;
+	node_list head = (node_list*)malloc(sizeof(struct NODE));
+	node_list last = (node_list*)malloc(sizeof(struct NODE));
+	head->next = last;
+	scanf("%d %d", &n, &m);
+	
+	last->data = n;
+	
+	for(i=n-1; i>=1; i--)
+	{
+		addNode(head, i);
+	}
+	last->next = head->next;
+	
+	node_list curr = head;
+	
+	printf("<");
+	for(i=0; i<7; i++)
+	{
+		for(j=0; j<m-1; j++)
+		{
+			curr=curr->next;
+		}
+		if(i<6)
+			printf("%d, ", curr->next->data);
+		else
+			printf("%d", curr->next->data);
+		removeNode(curr);
+		
+	}
+	printf(">");
+
+	return 0;
+}
